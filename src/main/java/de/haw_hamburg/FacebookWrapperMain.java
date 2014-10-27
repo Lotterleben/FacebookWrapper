c2cfcc6a2b8931e21ba3fb5c02f22a01f147f90e
raimundwege <raimundwege@gmail.com>
2014-10-27 17:42:54 +0100

    initial commit

---
+++ src/main/java/de/haw_hamburg/FacebookWrapperMain.java
@@ -0,0 +1,12 @@
+package de.haw_hamburg;
+
+public class FacebookWrapperMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Good bye world!");
+	}
+
+}
