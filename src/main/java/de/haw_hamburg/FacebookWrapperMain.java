9209b84f75e4b239a37a1160dc52f01c0f61f833
Lotte Steenbrink <lotte@zombietetris.de>
2014-10-28 00:19:24 +0100

    screw facebook4j, we're DIYing this now.

--- src/main/java/de/haw_hamburg/FacebookWrapperMain.java
+++ src/main/java/de/haw_hamburg/FacebookWrapperMain.java
@@ -1,12 +1,15 @@
 package de.haw_hamburg;
 
+
 public class FacebookWrapperMain {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Good bye world!");
+		System.out.println("Ohai!");
+        HTTPHandler httpHandler = new HTTPHandler();
+        httpHandler.get();
 	}
 
 }
---
+++ src/main/java/de/haw_hamburg/HTTPHandler.java
@@ -0,0 +1,45 @@
+package de.haw_hamburg;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+
+/**
+ * Created by lotte on 28.10.14.
+ */
+public class HTTPHandler {
+    static HttpClient client;
+
+    public HTTPHandler () {
+        client = new DefaultHttpClient();
+    }
+
+    public void get() {
+        try {
+            HttpGet request = new HttpGet("http://graph.facebook.com/1509447019293978?fields=id,name");
+            HttpResponse response = client.execute(request);
+
+            BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        } catch (HttpException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
